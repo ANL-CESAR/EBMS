e83197ae0a82b23c506a6e4ddfe8fdfba81c5b5d
Junchao Zhang <jczhang@mcs.anl.gov>
2015-04-01 07:44:18 -0500

    Added an EBMC code with MPI3 shared memory and MPI_Iallgather

---
+++ ebmc.c
@@ -0,0 +1,465 @@
+#include "matrix.h"
+#include "mpi.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<unistd.h>
+#include<signal.h>
+#include <string.h>
+#include <unistd.h>
+
+#define rn() (rand())/((double) RAND_MAX)
+
+#define MAX_FILENAME_CHARS = 128
+typedef enum boolean{
+    FALSE,
+    TRUE
+} Boolean;
+
+typedef struct range{
+    float lower;
+    float upper;
+} Range;
+
+typedef struct proc{
+    int lrank;
+    int grank;
+    int type;
+} Proc;
+
+
+typedef struct Particle_type{
+    int band;
+    double energy;
+    Boolean absorbed;
+    int proc;
+} Particle;
+
+
+typedef struct {
+    double val;
+    int rank;
+}  DoubleIntPair;
+
+float **matrix(long nrl, long nrh, long ncl, long nch);
+void create_scattering_matrix(float **scattering_matrix, int nb);
+void read_scattering_matrix(char* sm_file, float **scattering_matrix, int nb);
+void init_xsdata(float *xsdata, int nl);
+void init_particles(Particle *p, long npl, int mype);
+void set_energy_ranges(Range*, int);
+long tot(long f[], long n);
+double sum(double* f, int n);
+void process_input(int argc, char **argv,
+    int* nm, int* nb,
+    long* gsizeb,
+    long* npg,
+    float* tracking_rate, int *use_file,
+    char *sm_file);
+
+/*-----------------------------------------*/
+void read_scattering_matrix(char *sm_file, float **scattering_matrix, int nb){
+    int i,j, n, m;
+    float val;
+    FILE * file;
+
+    file=fopen(sm_file,"r");
+    fscanf(file,"%d",&n);
+    fscanf(file,"%d",&m);
+    assert (n == nb);
+    assert (n == m);
+
+    for(i=0;i<n;i++) {
+      for(j=0;j<m;j++) {
+        fscanf(file,"%f",&val);
+        scattering_matrix[i][j] = val;
+        printf("%f ", scattering_matrix[i][j]);
+      }
+      printf("\n");
+    }
+
+    fclose(file);
+}
+/* END MAIN */
+
+
+/*---------------------------------------------------
+    - void init_xsdata(inout xsdata, in nl)
+    intialiaze cross section data. for purposes
+    of this kernel app the values are arbitrary, only
+    size of data structure matters.
+    ------------------------------------------------------*/
+void init_xsdata(float *xsdata, int nl){
+    int i;
+    for (i = 0; i < nl; ++i)
+        xsdata[i] = 1.0;
+}
+
+
+void init_particles(Particle *p, long npl, int mype){
+    long i;
+    for (i=0;i<npl;++i){
+      p[i].energy = 2.0;
+      p[i].absorbed = FALSE;
+      p[i].band = 0;
+      p[i].proc = mype;
+    }
+}
+
+void set_energy_ranges(Range *energy_ranges, int nm){
+    int i;
+    float delta = 2.0/nm;
+    energy_ranges[0].lower=0.0;
+    energy_ranges[0].upper=delta;
+    for (i=1;i<nm;++i){
+      energy_ranges[i].lower = energy_ranges[i-1].upper;
+      energy_ranges[i].upper = energy_ranges[i].lower + delta;
+    }
+}
+
+long tot(long f[], long n){
+    long sum = 0;
+    int i;
+    for (i=0;i<n;++i)
+      sum += f[i];
+    return sum;
+}
+
+double sum(double* f, int n){
+    double sum = 0;
+    int i;
+    for (i=0;i<n;++i)
+      sum += f[i];
+    return sum;
+}
+
+void create_scattering_matrix(float **scattering_matrix, int nb){
+    //create pdf
+    for (int i = 0; i < nb; ++i) {
+        for (int j = 0; j < nb; ++j) {
+            if (i > j)
+                scattering_matrix[i][j] = 0.0;
+            else
+                scattering_matrix[i][j] = 1.0/(nb-i);
+        }
+    }
+
+    //convert to cdf for mini-app
+    for (int i = 0; i < nb; ++i) {
+        for (int j = i + 1; j < nb; ++j) {
+            scattering_matrix[i][j] += scattering_matrix[i][j-1];
+        }
+        scattering_matrix[i][nb-1] = 1.0; /* So as to avoid roundoff error */
+    }
+}
+
+int main(int argc, char **argv)
+{
+    long  npg;                // global number of particles
+    int   r;                 // number of nodes per memory group
+    int   nb;                 // num energy bands
+    long  npl;                // local number of particles on each proc
+    long  gsizekb;            // global size of xs data in kilobytes
+    float tracking_rate;      // empirical tracking rate (particles/sec)
+    Range *ranges;            // max and min energy for each band
+    float **scattering_matrix;// intra-group scattering probability, or size nb x nb
+    float *xsdata;     // cross section data (mimicked)
+    char *buf1, *buf2; // Double buffering to receive remote xsdata
+    char *xscomm; // Point to buf1 or buf2. It is the receive buf for a pending communication
+    char *xswork; // Contains xs data we can use for computation. It may point to
+                  // buf1, buf2, or a band in xsdata[] if the band is local
+    MPI_Request req1 = MPI_REQUEST_NULL;
+    MPI_Request req2 = MPI_REQUEST_NULL;
+    MPI_Request *reqcomm, *reqwork; // point to req1 or req2
+    int flip = 1; // choosing between xscomm/xswork, reqcomm/reqwork
+
+    Particle *p;              // local list of particles
+    char     sm_file[128];    // path to file containing scattering matrix
+    int use_file = FALSE;
+
+    MPI_Datatype dtype_kbytes; // An MPI contiguous datatype of 1024 bytes
+    long my_total_alive, global_total_alive, *n_alive;
+    double ts, tf, ttot, tave, tmax;
+    double ran_val, probability;
+
+    MPI_Comm shmcomm, dsmcomm;
+    int my_global_rank, nprocs;
+    int my_shm_rank, my_shm_size;
+    int my_dsm_rank, my_dsm_size;
+    int rc;
+    MPI_Aint size;
+    int disp_unit;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_global_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    // Creating a big data type so that the count argument in MPI_Send/Recv/Bcast
+    // won't run out of range of an integer, i.e., 2GB
+    MPI_Type_contiguous(1024, MPI_CHAR, &dtype_kbytes);
+    MPI_Type_commit(&dtype_kbytes);
+
+
+    // Rank 0 reads user input and then broadcast it
+    fflush(stdout);
+    if (my_global_rank == 0) {
+        process_input(argc, argv, &r, &nb, &gsizekb, &npg,
+		              &tracking_rate, &use_file, sm_file);
+    }
+
+    MPI_Bcast(&r, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&nb, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&gsizekb, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&npg, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&tracking_rate, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    assert (nb >= r);
+
+    npl = npg / nprocs;
+    if (my_global_rank < npg % nprocs) npl++;
+
+    // init scattering matrix on rank 0 and broadcast
+    scattering_matrix = matrix(0,nb-1,0,nb-1);
+    if (my_global_rank == 0){
+        if (use_file == TRUE){
+            read_scattering_matrix(sm_file,scattering_matrix,nb);
+        } else {
+            create_scattering_matrix(scattering_matrix,nb);
+        }
+    }
+    MPI_Bcast(&scattering_matrix[0][0], nb*nb, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    assert ((n_alive = (long *) malloc(nb*sizeof(long))) != NULL);
+
+    // set energy ranges associated with EACH BAND (min..max). This
+    // isn't really needed in this communication kernel but is included
+    // for conceptual purposes in case we choose to extend kernel
+    assert( (ranges =(Range *) malloc(nb*sizeof(Range))) != NULL);
+    set_energy_ranges(ranges,nb);
+
+    // allocate local list of particles and initialize all to highest
+    // energy band (ie band 0). Exact energies are not important for this
+    // kernel, only band.
+    assert( (p = (Particle *) malloc(npl*sizeof(Particle))) != NULL);
+    init_particles(p, npl, my_global_rank);
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Making various communicators out of MPI_COMM_WORLD.
+    //
+    // * shmcomm: it is a communicator for processes in a node. Processes in
+    //     a shmcomm can share memory. There is such a communicator per node.
+    // * shm_leader_comm: It is a communicator containing leaders (rank 0's)
+    //     of all shmcomm's. There is only one such communicator in the world.
+    // * dsmcomm: It is a communicator containing leaders (rank 0's) of
+    //      r shmcomm's. There is such a communicator per r nodes.
+    ////////////////////////////////////////////////////////////////////////////
+    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, my_global_rank, MPI_INFO_NULL, &shmcomm);
+    MPI_Comm_rank(shmcomm, &my_shm_rank);
+    MPI_Comm_size(shmcomm, &my_shm_size);
+
+    int is_shm_leader = (my_shm_rank == 0) ? 1 : 0;
+    int color = is_shm_leader ? 0 : MPI_UNDEFINED;
+    MPI_Comm shm_leader_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, color, my_global_rank, &shm_leader_comm);
+
+    if (is_shm_leader) {
+        int num_shms;
+        int shm_idx;
+        MPI_Comm_size(shm_leader_comm, &num_shms);
+
+        if (my_global_rank == 0) printf("num_shms = %d\n", num_shms);
+        assert(num_shms % r == 0); /* TODO: relax this assumption */
+
+        MPI_Comm_rank(shm_leader_comm, &shm_idx);
+        MPI_Comm_split(shm_leader_comm, shm_idx/r, shm_idx, &dsmcomm);
+    }
+
+    // Compute dsm rank and size
+    int tmp[2];
+    if (is_shm_leader) {
+        MPI_Comm_rank(dsmcomm, &my_dsm_rank);
+        MPI_Comm_size(dsmcomm, &my_dsm_size);
+        tmp[0] = my_dsm_rank;
+        tmp[1] = my_dsm_size;
+    }
+
+    MPI_Bcast(tmp, 2, MPI_INT, 0, shmcomm);
+    my_dsm_rank = tmp[0];
+    my_dsm_size = tmp[1];
+
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    // Every node allocates bands it owns in shared memory
+    //
+    ////////////////////////////////////////////////////////////////////////////
+
+    // For simplicity, assume xsdata can be evenly divided in nb bands
+    assert(gsizekb%nb == 0);
+    long long bandsizekb = gsizekb/nb;
+
+    // For simplicity, assume a band can be evenly distributed on r nodes
+    assert(bandsizekb % r == 0);
+    long long slicesizekb = bandsizekb/r; // slice size in kilobytes
+
+    // SHM leader allocates xsdata in its memory, since only leaders do communication.
+    // All other processes in the node only touch data in buf1 or buf2 (see below).
+    if (is_shm_leader) {
+        xsdata = (float*)malloc(slicesizekb*1024*nb);
+        assert(xsdata);
+    }
+
+    if (is_shm_leader && my_dsm_rank == 0) {
+        for (int i = 0; i < nb; i++) {
+            int *p = (int *) ((char*)xsdata + slicesizekb*1024*i);
+            *p = i;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    // Every node allocates the two buffers in shared memory
+    //
+    ////////////////////////////////////////////////////////////////////////////
+    // For simplicity, assume a band can evenly distributed on cores in a shm.
+    // buf1/2 are divided by processes in a shm to even out numa-effect.
+    MPI_Win shmwin1, shmwin2;
+    assert(bandsizekb*1024 % my_shm_size == 0);
+    MPI_Win_allocate_shared(bandsizekb*1024/my_shm_size, 1, MPI_INFO_NULL, shmcomm, &buf1, &shmwin1);
+    MPI_Win_allocate_shared(bandsizekb*1024/my_shm_size, 1, MPI_INFO_NULL, shmcomm, &buf2, &shmwin2);
+
+    // Query the start address of the buf on rank 0 of shmcomm
+    MPI_Win_shared_query(shmwin1, 0, &size, &disp_unit, &buf1);
+    MPI_Win_shared_query(shmwin2, 0, &size, &disp_unit, &buf2);
+
+    ////////////////////////////////////////////////////////////////////////////
+    //
+    // Start tracking partices
+    //
+    ////////////////////////////////////////////////////////////////////////////
+
+    // need to count how many alive in each band so
+    // that we don't load memory on subsequent passes
+    // when there are none alive. On first pass all neutrons are in band 0
+    n_alive[0] = npl;
+    for (int i=1; i<nb; ++i) n_alive[i] = 0;
+
+    global_total_alive = npg;
+    my_total_alive = npl;
+
+    ts = MPI_Wtime();
+    int trips = 0;
+    const double absorption_threshold = 1.0/nb;
+    while (global_total_alive > 0) {
+        trips++;
+        // Start the pipepline by communicating band 0
+        flip = 1;
+        if (is_shm_leader) {
+            char* sendbuf = (char*)xsdata;
+            xscomm = buf1;
+            reqcomm = &req1;
+            MPI_Iallgather(sendbuf, slicesizekb, dtype_kbytes, xscomm, slicesizekb, dtype_kbytes, dsmcomm, reqcomm);
+        }
+
+        // loop over bands starting with highest energy
+        for (int k = 0; k < nb; k++) {
+            // SHM mebmers align here since the SHM leader
+            // will receive band k+1 and overwrite one of the two bufs. We need to
+            // make sure all SHM members are done with the data in the buf.
+            MPI_Barrier(shmcomm);
+
+            // SHM leader starts communicating band k + 1 within DSM
+            if (is_shm_leader && (k+1 < nb)) {
+                xscomm = flip ? buf2 : buf1;
+                reqcomm = flip ? &req2 : &req1;
+                char* sendbuf = (char*)xsdata + slicesizekb*1024*(k+1);
+                MPI_Iallgather(sendbuf, slicesizekb, dtype_kbytes, xscomm, slicesizekb, dtype_kbytes, dsmcomm, reqcomm);
+            }
+
+            // Get xswork and then reverse
+            reqwork = flip ? &req1 : &req2;
+            xswork = flip ? buf1 : buf2;
+            flip = !flip;
+
+            // SHM members wait for their leader to complete nonblocking comm.
+            // Question: Can MPI_Barrier gurantee memory sync within SHM?
+            if (is_shm_leader) MPI_Wait(reqwork, MPI_STATUS_IGNORE);
+            MPI_Barrier(shmcomm);
+
+            assert(*(int*)xswork == k); // Make sure we are using correct cross sections
+
+            if (n_alive[k] != 0) { // skip if no alive particles in band k
+                for (int i=0; i< npl; ++i) {
+                    while (p[i].band == k && !p[i].absorbed) {
+                        ran_val = rn();
+                        //if (ran_val <= ABSORPTION_THRESHOLD) {
+                        if (ran_val <= absorption_threshold) {
+                            usleep( (1.0/tracking_rate)*1000000 ); // sleep in microseconds
+                            p[i].absorbed = TRUE;
+                            --n_alive[k];
+                        } else {
+                            ran_val = rn();
+                            int j = k;
+                            probability = scattering_matrix[k][j];
+                            while (ran_val > probability) {
+                                ++j;
+                                probability = scattering_matrix[k][j];
+                            }
+                            p[i].band = j; // move particle to band to which it was scattered
+                            --n_alive[k]; ++n_alive[j]; // adjust band-dependent alive counts
+                        }
+                    }
+                    // To make progress of nonblocking comm. of band k+1
+                    int flag;
+                    if (is_shm_leader && i % 64 == 0) MPI_Test(reqcomm, &flag, MPI_STATUS_IGNORE);
+                }
+            }
+        }
+        my_total_alive = tot(n_alive, nb);  // total alive across all bands after one sweep
+        MPI_Allreduce(&my_total_alive, &global_total_alive, 1, MPI_LONG, MPI_SUM, MPI_COMM_WORLD);
+    }
+    tf = MPI_Wtime();
+    ttot = tf - ts;
+
+    DoubleIntPair in, minout, maxout;
+    in.val = ttot;
+    in.rank = my_global_rank;
+    MPI_Reduce(&ttot, &tave, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&in, &maxout, 1, MPI_DOUBLE_INT, MPI_MAXLOC, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&in, &minout, 1, MPI_DOUBLE_INT, MPI_MINLOC, 0, MPI_COMM_WORLD);
+
+    if (my_global_rank== 0){
+        printf("nprocs = %d\n", nprocs);
+        printf("npg = %d million\n", (int)(npg/1000000.0));
+        printf("xsdata = %dGB\n", (int)(gsizekb/1048576.0));
+        printf("energy bands per DSM(i.e., nb) = %d\n", nb);
+        printf("energy bands per node = %d\n", nb/r);
+        printf("nodes in a DSM (i.e., r) = %d\n", r);
+        printf("message size per ibcast = %d(MB)\n\n", (int)(bandsizekb/1024));
+
+        if (use_file)
+            printf("scattering matrix file = %s\n", sm_file);
+        else
+            printf("scattering matrix is self-created\n");
+
+        printf("input tracking rate = %.1f particles/s\n", tracking_rate);
+        printf("Go through the energy bands %d times\n", trips);
+        printf("Average total tracking time(s)         = %.6f\n", tave/nprocs);
+        printf("Minimal total tracking time on rank %d = %.6f\n", minout.rank, minout.val);
+        printf("Maximal total tracking time on rank %d = Tebmc(s) = %.6f\n", maxout.rank, maxout.val);
+        printf("Tclassic(s)                            = %.1f\n", npg/tracking_rate/nprocs);
+        printf("Tebmc/Tclassic                         = %.1f\n", maxout.val/(npg/tracking_rate/nprocs));
+        fflush(stdout);
+    }
+
+    free(n_alive);
+    MPI_Comm_free(&shmcomm);
+    if (is_shm_leader) {
+        MPI_Comm_free(&shm_leader_comm);
+        MPI_Comm_free(&dsmcomm);
+    }
+    MPI_Win_free(&shmwin1);
+    MPI_Win_free(&shmwin2);
+    //TODO: also free scattering_matrix
+    MPI_Type_free(&dtype_kbytes);
+    MPI_Finalize();
+    return 0;
+}
